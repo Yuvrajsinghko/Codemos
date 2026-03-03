f4ce11c7d0ef1fb828f0d3c11f6c1ce03fad8b8b
Yuvrajsinghko <yuvrajprince19@gmail.com>
2026-03-03 23:41:55 +0530

    added new fonts and installed GSAP and tailwindcss

---
+++ src/constants.js
@@ -0,0 +1,27 @@
+ const logos = [
+  "/logos/appointy-Logo.svg",
+  "/logos/calsoft-logo.png",
+  "/logos/cognam-logo.png",
+  "/logos/data-genie-logo.png",
+  "/logos/enqurious-logo.png",
+  "/logos/fico-logo.png",
+  "/logos/gamaEdge-logo.png",
+  "/logos/infinix-1.svg",
+  "/logos/moengage-logo.svg",
+  "/logos/tech-mahindra-logo.svg",
+  "/logos/valueLab-logo.svg",
+];
+
+const features = [
+  "Personalized learning tracks",
+  "100% placement guarantee",
+  "one to one mentorship",
+  "skill based profile creation",
+  "real life projects",
+  "paid internship",
+  "certification programme",
+  "corporate training",
+];
+
+export default {logos,features}
+
