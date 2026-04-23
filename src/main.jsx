360908f0628941aa9b6c84cc7b883328e29c27f8
Aman <amanraikwar1996@gmail.com>
2026-04-23 21:13:52 +0530

    feat: add multi-page routing and global layout with Navbar & Footer
    - Integrated react-router-dom for client-side routing
    - Created separate pages: Home, Courses, Placements, About
    - Moved existing section-based layout into Home page
    - Updated App.jsx to use BrowserRouter, Routes, and Route
    - Implemented global layout with Navbar and Footer across all pages
    - Ensured consistent UI and styling across all routes.

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <div className='selection:bg-yellow-400 selection:text-black'>
-    <App />
-  </div>,
+  <BrowserRouter>
+    <div className='selection:bg-yellow-400 selection:text-black'>
+      <App />
+    </div>,
+  </BrowserRouter>
 )
---
+++ src/pages/PlacementsPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const PlacementsPage = () => {
+    return (
+		<>
+			<div className="text-white text-4xl p-20">
+				Placements Page
+			</div>
+		</>
+    )
+}
+
+export default PlacementsPage
