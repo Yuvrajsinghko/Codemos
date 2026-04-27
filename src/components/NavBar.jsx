4777e1e9508817372cb12a3c70697b51a1dbf061
Aman <amanraikwar1996@gmail.com>
2026-04-27 19:56:35 +0530

    feat: refine navbar layout with improved alignment, spacing and styling.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,10 +2,12 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
 	return (
-		<nav className="text-white w-screen flex justify-between items-center px-8 py-8">
-			<Link to="/" className="text-3xl font-bold font-[NeueMachina]">CODEMOS</Link>
+		<nav className="text-white w-screen flex items-center px-10 py-6">
+			<div className="flex-1">
+				<Link to="/" className="text-2xl font-bold font-[NeueMachina]">CODEMOS</Link>
+			</div>
 
-			<div className="hidden md:flex gap-8 text-lg outline outline-amber-400 py-5 px-10 rounded-xl">
+			<div className="hidden md:flex gap-8 text-md outline outline-amber-400 py-3 px-8 rounded-lg">
 				<Link to="/" className="cursor-pointer hover:text-amber-500">Home</Link>
 
 				<Link to="/courses" className="cursor-pointer hover:text-amber-500">Courses</Link>
@@ -15,11 +17,13 @@ const Navbar = () => {
 				<Link to="/about" className="cursor-pointer hover:text-amber-500">About Us</Link>
 			</div>
 
-			<button className="bg-amber-500 text-lg font-bold font-[NeueMachina] rounded-xl px-4 py-2">
-				Request Callback
-			</button>
+			<div className="flex-1 flex justify-end">
+				<button className="bg-amber-500 text-md font-bold font-[NeueMachina] rounded-lg px-4 py-2">
+					Request Callback
+				</button>
+			</div>
 		</nav>
 	);
 };
 
-export default Navbar;
+export default Navbar; 
