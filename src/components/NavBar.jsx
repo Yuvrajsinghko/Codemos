f43f5fb7759fd5c8dce52033fa2bcf42f8df0834
Yuvraj Singh <yuvrajprince19@gmail.com>
2026-04-28 22:49:27 +0530

    feat(ui): add reusable Modal component

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,29 +1,57 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import CallbackModal from "./CallbackModal";
 
 const Navbar = () => {
-	return (
-		<nav className="text-white w-screen flex items-center px-10 py-6">
-			<div className="flex-1">
-				<Link to="/" className="text-2xl font-bold font-[NeueMachina]">CODEMOS</Link>
-			</div>
+  const [showModal, setShowModal] = useState(false);
 
-			<div className="hidden md:flex gap-8 text-md outline outline-amber-400 py-3 px-8 rounded-lg">
-				<Link to="/" className="cursor-pointer hover:text-amber-500">Home</Link>
+  const handleToggleModal = () => {
+    setShowModal(!showModal);
+  };
 
-				<Link to="/courses" className="cursor-pointer hover:text-amber-500">Courses</Link>
+  const closeModal = ()=>{
+	setShowModal(false);
+  }
 
-				<Link to="/placements" className="cursor-pointer hover:text-amber-500">Placements</Link>
+  return (
+    <nav className="text-white w-screen flex items-center px-10 py-6">
+      <div className="flex-1">
+        <Link to="/" className="text-2xl font-bold font-[NeueMachina]">
+          CODEMOS
+        </Link>
+      </div>
 
-				<Link to="/about" className="cursor-pointer hover:text-amber-500">About Us</Link>
-			</div>
+      <div className="hidden md:flex gap-8 text-md outline outline-amber-400 py-3 px-8 rounded-lg">
+        <Link to="/" className="cursor-pointer hover:text-amber-500">
+          Home
+        </Link>
 
-			<div className="flex-1 flex justify-end">
-				<button className="bg-amber-500 text-md font-bold font-[NeueMachina] rounded-lg px-4 py-2">
-					Request Callback
-				</button>
-			</div>
-		</nav>
-	);
+        <Link to="/courses" className="cursor-pointer hover:text-amber-500">
+          Courses
+        </Link>
+
+        <Link to="/placements" className="cursor-pointer hover:text-amber-500">
+          Placements
+        </Link>
+
+        <Link to="/about" className="cursor-pointer hover:text-amber-500">
+          About Us
+        </Link>
+      </div>
+
+      <div className="flex-1 flex justify-end">
+        <button
+          onClick={handleToggleModal}
+          className="bg-amber-500 text-md font-bold font-[NeueMachina] rounded-lg px-4 py-2"
+        >
+          Request Callback
+        </button>
+		{
+			showModal && <CallbackModal onClose={closeModal}  />
+		}
+      </div>
+    </nav>
+  );
 };
 
-export default Navbar; 
+export default Navbar;
